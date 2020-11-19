d52fa30f9f78152abf39cf8c8eac418bbf3c9a8a
yoneyan <github@yoneyan.net>
2020-11-20 04:22:02 +0900

    Tool関係のファイルの追加

---
+++ pkg/api/core/tool/config/config.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Config struct {
+	Controller Controller `json:"controller"`
+	Node       Imacon     `json:"imacon"`
+	DB         DB         `json:"db"`
+	Storage    []Storage  `json:"storage"`
+}
+
+type Controller struct {
+	IP   string `json:"ip"`
+	Port int    `json:"port"`
+	Auth Auth   `json:"auth"`
+}
+
+type Auth struct {
+	Token1 string `json:"token1"`
+	Token2 string `json:"token2"`
+	Token3 string `json:"token3"`
+}
+
+type Imacon struct {
+	Url  string `json:"url"`
+	Port int    `json:"port"`
+}
+
+type AdminAuth struct {
+	User string `json:"user"`
+	Pass string `json:"pass"`
+}
+
+type DB struct {
+	IP     string `json:"ip"`
+	Port   int    `json:"port"`
+	User   string `json:"user"`
+	Pass   string `json:"pass"`
+	DBName string `json:"dbName"`
+}
+
+type Storage struct {
+	ID   uint   `json:"id"`
+	Type uint   `json:"type"`
+	Path string `json:"path"`
+}
+
+var Conf Config
+
+func GetConfig(inputConfPath string) error {
+	configPath := "./data.json"
+	if inputConfPath != "" {
+		configPath = inputConfPath
+	}
+	file, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return err
+	}
+	var data Config
+	json.Unmarshal(file, &data)
+	Conf = data
+	return nil
+}
