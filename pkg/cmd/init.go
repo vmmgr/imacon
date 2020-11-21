d63941e6981dcb3ab3b2af5ccf72d670d4181512
yoneyan <github@yoneyan.net>
2020-11-22 02:51:23 +0900

    config読み込みの修正

--- pkg/cmd/init.go
+++ pkg/cmd/init.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"github.com/jinzhu/gorm"
 	"github.com/spf13/cobra"
+	"github.com/vmmgr/imacon/pkg/api/core/storage"
+	"github.com/vmmgr/imacon/pkg/api/core/tool/config"
+	"log"
+	"strconv"
 )
 
 // initCmd represents the init command
@@ -18,6 +23,24 @@ var initDBCmd = &cobra.Command{
 	Short: "store init",
 	Long:  "store init cmd",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		confPath, err := cmd.Flags().GetString("config")
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+			return err
+		}
+		if config.GetConfig(confPath) != nil {
+			log.Fatalf("error config process |%v", err)
+			return err
+		}
+
+		db, err := gorm.Open("mysql", config.Conf.DB.User+":"+config.Conf.DB.Pass+"@"+
+			"tcp("+config.Conf.DB.IP+":"+strconv.Itoa(config.Conf.DB.Port)+")"+"/"+config.Conf.DB.DBName+"?parseTime=true")
+		if err != nil {
+			return err
+		}
+		result := db.AutoMigrate(&storage.Storage{})
+		log.Println(result.Error)
+		log.Println("end")
 		return nil
 	},
 }
